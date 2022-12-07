0465775a02609f30c77ccdc1660d9c6728969746
Willian Dutra <contatowilliandutra@gmail.com>
2022-12-07 18:41:38 -0300

    Requisito 30 - Caso receita já em andamento o texto do botão muda

--- src/components/RecipeButton.js
+++ src/components/RecipeButton.js
@@ -2,7 +2,8 @@ import { useEffect, useContext } from 'react';
 import RecipesContext from '../context/RecipesContext';
 
 export default function RecipeButton() {
-  const { recipeDetails, recipeDone, setRecipeDone } = useContext(RecipesContext);
+  const { recipeDetails, recipeDone, setRecipeDone,
+    inProgress, setInProgress } = useContext(RecipesContext);
 
   useEffect(() => {
     const checkDoneRecipes = () => {
@@ -20,6 +21,29 @@ export default function RecipeButton() {
     checkDoneRecipes();
   }, [recipeDetails, setRecipeDone]);
 
+  useEffect(() => {
+    const checkInProgress = () => {
+      const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
+
+      if (typeof inProgressRecipes === 'object' && inProgressRecipes !== null) {
+        if (recipeDetails.meals) {
+          const id = recipeDetails.meals[0].idMeal;
+          const isInProgress = inProgressRecipes.meals[id];
+          return isInProgress && setInProgress(true);
+        } if (recipeDetails.drinks) {
+          const id = recipeDetails.drinks[0].idDrink;
+          const isInProgress = inProgressRecipes.drinks[id];
+          return isInProgress && setInProgress(true);
+        }
+        return setInProgress(false);
+      }
+    };
+
+    if (typeof recipeDetails === 'object') {
+      checkInProgress();
+    }
+  }, [recipeDetails, setInProgress]);
+
   return (
     <>
       { !recipeDone && (
@@ -28,7 +52,7 @@ export default function RecipeButton() {
           className="start-btn"
           data-testid="start-recipe-btn"
         >
-          Start Recipe
+          { inProgress ? 'Continue Recipe' : 'Start Recipe'}
         </button>
       )}
       <div />
