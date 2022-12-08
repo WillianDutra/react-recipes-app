af237d51e27e0548484006ae62882455f02bb8e0
Marinna Vitoriano <marinna@outlook.com>
2022-12-08 16:26:09 -0300

    requisito 14 finalizado

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -25,9 +25,27 @@ function Drinks() {
   };
 
   return (
-    <>
-      <div>
-        { recipes.length > 1
+    <main>
+      <div className="filter-buttons">
+        { drinksFilters.slice(0, filters).map((ele) => (
+          <button
+            key={ ele.strCategory }
+            data-testid={ `${ele.strCategory}-category-filter` }
+            type="button"
+            onClick={ ({ target: { innerText } }) => getRecipes(innerText) }
+          >
+            {ele.strCategory}
+          </button>
+        ))}
+        <button
+          data-testid="All-category-filter"
+          type="button"
+          onClick={ () => setCategoryActive({ active: false, category: '' }) }
+        >
+          All
+        </button>
+      </div>
+      { recipes.length > 1
       && recipes.slice(0, maxRecipes).map((ele, index) => (
         <div
           key={ ele.idDrink }
@@ -48,53 +66,28 @@ function Drinks() {
           </Link>
         </div>
       ))}
-      </div>
-      <main>
-        {!categoryActive.active
-        && (
-          <div className="filter-buttons">
-            { drinksFilters.slice(0, filters).map((ele) => (
-              <button
-                key={ ele.strCategory }
-                data-testid={ `${ele.strCategory}-category-filter` }
-                type="button"
-                onClick={ ({ target: { innerText } }) => getRecipes(innerText) }
+      { (categoryActive.active ? drinksByCategory : drinksRequest)
+        .slice(0, maxRecipes).map((ele, index) => (
+          <div
+            key={ ele.idDrink }
+            className="card"
+            data-testid={ `${index}-recipe-card` }
+          >
+            <Link to={ `/drinks/${ele.idDrink}` }>
+              <img
+                src={ ele.strDrinkThumb }
+                alt={ ele.strDrink }
+                data-testid={ `${index}-card-img` }
+              />
+              <p
+                data-testid={ `${index}-card-name` }
               >
-                {ele.strCategory}
-              </button>
-            ))}
-            <button
-              data-testid="All-category-filter"
-              type="button"
-              onClick={ () => setCategoryActive({ active: false, category: '' }) }
-            >
-              All
-            </button>
+                { ele.strDrink }
+              </p>
+            </Link>
           </div>
-        )}
-        { (categoryActive.active ? drinksByCategory : drinksRequest)
-          .slice(0, maxRecipes).map((ele, index) => (
-            <div
-              key={ ele.idDrink }
-              className="card"
-              data-testid={ `${index}-recipe-card` }
-            >
-              <Link to={ `/drinks/${ele.idDrink}` }>
-                <img
-                  src={ ele.strDrinkThumb }
-                  alt={ ele.strDrink }
-                  data-testid={ `${index}-card-img` }
-                />
-                <p
-                  data-testid={ `${index}-card-name` }
-                >
-                  { ele.strDrink }
-                </p>
-              </Link>
-            </div>
-          ))}
-      </main>
-    </>
+        ))}
+    </main>
   );
 }
 
