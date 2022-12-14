bc187da7b4532ce01cb14fea54ef73d6e797cc36
Willian Dutra <contatowilliandutra@gmail.com>
2022-12-14 18:38:58 -0300

    Estilização da página de perfil

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,11 @@ import { useHistory } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
+import doneIcon from '../images/doneRecipeIcon.svg';
+import favIcon from '../images/favoriteIcon.svg';
+import logoutIcon from '../images/logoutIcon.svg';
+import '../styles/profile.css';
+
 function Profile() {
   const e = JSON.parse(localStorage.getItem('user'));
   const history = useHistory();
@@ -23,28 +28,41 @@ function Profile() {
   return (
     <>
       <Header />
-      <h2 data-testid="profile-email">{ e ? e.email : 'Email not found' }</h2>
-      <button
-        type="button"
-        onClick={ redirectToPageDoneRecipes }
-        data-testid="profile-done-btn"
+      <h2
+        data-testid="profile-email"
+        className="email"
       >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        onClick={ redirectToPageFavoriteRecipes }
-        data-testid="profile-favorite-btn"
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        onClick={ logOut }
-        data-testid="profile-logout-btn"
-      >
-        Logout
-      </button>
+        { e ? e.email : 'Email not found' }
+      </h2>
+      <div className="links-container">
+        <button
+          type="button"
+          onClick={ redirectToPageDoneRecipes }
+          data-testid="profile-done-btn"
+        >
+          <img src={ doneIcon } alt="done-icon" />
+          Done Recipes
+        </button>
+        <hr />
+        <button
+          type="button"
+          onClick={ redirectToPageFavoriteRecipes }
+          data-testid="profile-favorite-btn"
+        >
+          <img src={ favIcon } alt="fav-icon" />
+
+          Favorite Recipes
+        </button>
+        <hr />
+        <button
+          type="button"
+          onClick={ logOut }
+          data-testid="profile-logout-btn"
+        >
+          <img src={ logoutIcon } alt="logout-icon" />
+          Logout
+        </button>
+      </div>
       <Footer />
     </>
   );
